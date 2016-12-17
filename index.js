e0b337c50c8ed95e7f23b0d219ebcc6e5bdd34ec
SlowBubble <chorhanglam@gmail.com>
2016-12-17 12:08:23 -0500

    added map and stops

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 
-function getQueryUrl(queryBy, routes, apiKey) {
-  return 'https://realtime.mbta.com/developer/api/v2/' + queryBy + '?routes=' + routes + '&format=json&api_key=' + apiKey;
+function getQueryUrl(queryBy, routesKey, routesVal, apiKey) {
+  return 'https://realtime.mbta.com/developer/api/v2/' + queryBy + '?' + routesKey + '=' + routesVal + '&format=json&api_key=' + apiKey;
 }
 
 var apiKey = 'SGED1xrcdUiBRkdRrq6zJQ';
 
 
-var redVehiclesUrl = getQueryUrl('vehiclesbyroutes', 'Red', apiKey); 
-var redPredictionsUrl = getQueryUrl('predictionsbyroutes', 'Red', apiKey);
+var redVehiclesUrl = getQueryUrl('vehiclesbyroutes', 'routes', 'Red', apiKey); 
+var redPredictionsUrl = getQueryUrl('predictionsbyroutes', 'routes', 'Red', apiKey);
+var redStopsUrl = getQueryUrl('stopsbyroute', 'route', 'Red', apiKey); 
 
 var north = 'Northbound';
 var south = 'Southbound';
+
+var vehicles = [];
+var stops = [];
 
 function getVehicles(vehiclesData) {
   vehicles = [];
@@ -28,7 +32,27 @@ function getVehicles(vehiclesData) {
   return vehicles;
 }
 
+$.get(redStopsUrl, function(data) {
+  data.direction[0].stop.forEach(function(stop) {
+    stop.lat = parseFloat(stop.stop_lat);
+    stop.lng = parseFloat(stop.lng);
+    stops.push(stop);
+  });
+  stop = stops[1];
+  initMap(parseFloat(stop.stop_lat), parseFloat(stop.stop_lon));
+});
+
 $.get(redVehiclesUrl, function(data, status, xhr){
   vehicles = getVehicles(data);
-  console.log(vehicles);
-}, 'json')
+}, 'json');
+
+/////////// Map
+var map;
+function initMap(latitude, longitude) {
+  latitude = latitude || 42.39674;
+  longitude = longitude || -71.121815;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    zoom: 15,
+  });
+}
